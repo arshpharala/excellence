e65005c0e93b6e2a76217d9692d23f8be0a2cd68
Arshdeep Singh <arshdeepjhagra@gmail.com>
2025-08-26 10:04:09 +0400

    changes

--- excellence/js/main.js
+++ excellence/js/main.js
@@ -9,28 +9,28 @@ import initScrollTop from "./scrollTopBtn.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   AOS.init({
-    duration: 1700,
+    duration: 1500,
     easing: "ease-in-out",
-      // once: true 
+    once: true,
   });
 
-  const observer = new IntersectionObserver(
-    (entries) => {
-      entries.forEach((entry) => {
-        if (entry.intersectionRatio >= 0.4) {
-          entry.target.classList.add("aos-animate");
-        }
-      });
-    },
-    { threshold: [0.4] }
-  );
+  // const observer = new IntersectionObserver(
+  //   (entries) => {
+  //     entries.forEach((entry) => {
+  //       if (entry.intersectionRatio >= 0.4) {
+  //         entry.target.classList.add("aos-animate");
+  //       }
+  //     });
+  //   },
+  //   { threshold: [0.4] }
+  // );
 
-  ["#process", "#footer"].forEach((section) => {
-    document.querySelectorAll(`${section} [data-aos]`).forEach((el) => {
-      el.classList.remove("aos-animate"); // reset AOS so observer controls it
-      observer.observe(el);
-    });
-  });
+  // ["#process", "#footer"].forEach((section) => {
+  //   document.querySelectorAll(`${section} [data-aos]`).forEach((el) => {
+  //     el.classList.remove("aos-animate"); 
+  //     observer.observe(el);
+  //   });
+  // });
   initHero();
   initScrollTop();
   initNavbar();
